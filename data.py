037a2549b014442af04fe96cca342b7ee6b5c79b
shakurdaye <shakurdaye25@gmail.com>
2025-07-12 18:16:46 -0400

    Add helper.py file

--- data.py
+++ data.py
@@ -1,1 +1,11 @@
 # File with constants
+#URL for the Urban Routes app
+URBAN_ROUTES_URL = 'https://cnt-dcf0e9f3-a1a5-430c-b573-e997f97881c9.containerhub.tripleten-services.com/'
+
+# Test Data
+ADDRESS_FROM = 'East 2nd street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juicer bar, please'
